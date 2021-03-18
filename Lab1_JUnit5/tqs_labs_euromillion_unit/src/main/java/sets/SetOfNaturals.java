34a02d958c55380767c0fdcaa9edff6946cb6780
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-03-18 11:35:03 +0000

    Lab1 2. b) Extra tests for SetOfNaturals

--- Lab1_JUnit5/tqs_labs_euromillion_unit/src/main/java/sets/SetOfNaturals.java
+++ Lab1_JUnit5/tqs_labs_euromillion_unit/src/main/java/sets/SetOfNaturals.java
@@ -42,6 +42,11 @@ public class SetOfNaturals implements Iterable<Integer> {
 
 
 	public boolean intersects(SetOfNaturals subset) {
+		for(Integer i:this.collection) {
+			if(subset.contains(i)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
--- Lab1_JUnit5/tqs_labs_euromillion_unit/src/test/java/sets/SetOfNaturalsTest.java
+++ Lab1_JUnit5/tqs_labs_euromillion_unit/src/test/java/sets/SetOfNaturalsTest.java
@@ -36,14 +36,16 @@ public class SetOfNaturalsTest {
 
     @Test
     public void testAddElement() {
-
         setA.add(99);
         assertTrue(setA.contains(99), "add: added element not found in set.");
-        assertEquals(1, setA.size());
+        assertEquals(1, setA.size(), "size: unexpected size after add.");
 
         setB.add(11);
         assertTrue(setB.contains(11), "add: added element not found in set.");
         assertEquals(7, setB.size(), "add: elements count not as expected.");
+
+        assertThrows(IllegalArgumentException.class, () -> setB.add(10), "add: duplicate number insert did not throw error.");
+
     }
 
     @Test
@@ -51,13 +53,32 @@ public class SetOfNaturalsTest {
         int[] elems = new int[]{10, 20, -30};
 
         // must fail with exception
-        assertThrows(IllegalArgumentException.class, () -> setA.add(elems));
+        assertThrows(IllegalArgumentException.class, () -> setA.add(elems), "add: not natural number (negative) insert did not throw error.");
     }
 
 
     @Test
     public void testIntersectForNoIntersection() {
         assertFalse(setA.intersects(setB), "no intersection but was reported as existing");
+        assertTrue(setB.intersects(setC), "intersection exists but not reported");
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(0, setA.size(), "size: empty set does not have size 0");
+    }
+
+    @Test
+    public void testContains() {
+        assertFalse(setA.contains(10), "contains: set does not contain but report it does");
+    }
+
+    @Test
+    public void testEquals() {
+        assertFalse(setA.equals(setB), "equals: different sets reported as equal");
+
+        setA.add(new int[]{10, 20, 30, 40, 50, 60});
+        assertTrue(setA.equals(setB), "equals: equal sets reported as different");
 
     }
 
