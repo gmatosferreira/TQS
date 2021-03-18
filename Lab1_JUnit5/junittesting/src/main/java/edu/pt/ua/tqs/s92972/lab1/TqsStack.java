3d0125d30ce579cf0f150162623194eee01c3200
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-03-18 10:54:28 +0000

    Lab1 1. Red-Green-Refactor on TqsStack

--- Lab1_JUnit5/junittesting/src/main/java/edu/pt/ua/tqs/s92972/lab1/TqsStack.java
+++ Lab1_JUnit5/junittesting/src/main/java/edu/pt/ua/tqs/s92972/lab1/TqsStack.java
@@ -2,21 +2,30 @@ package edu.pt.ua.tqs.s92972.lab1;
 
 import java.util.LinkedList;
 
+import java.lang.IllegalArgumentException;
+import java.lang.IllegalStateException;
+
 public class TqsStack<T> {
 
     private int bound;
     private LinkedList<T> list;
 
+    public TqsStack () {
+        this.list = new LinkedList<>();
+    }
+
     public TqsStack (int bound) {
+        if (bound<=0) {
+            throw new IllegalArgumentException("The stack bound must be a natural number (>0)!");
+        }
         this.bound = bound;
         this.list = new LinkedList<>();
     }
 
-    public TqsStack () {
-        this(0);
-    }
-
     public void push(T t) {
+        if (this.bound > 0 && this.list.size()+1 > this.bound) {
+            throw new IllegalStateException("The stack boundary has been reached!");
+        }
         list.add(0, t);
     }
 
