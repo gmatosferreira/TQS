ec69e4f1e775b28aec6a9fca6eed486d2689e05e
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-03-27 16:29:02 +0000

    Lab2 2. d) Testing findAddressForLocation() with mock

--- lab2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
+++ lab2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
@@ -1,6 +1,7 @@
 package geocoding;
 
 import connection.ISimpleHttpClient;
+import connection.TqsBasicHttpClient;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,28 +15,37 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AddressResolverTest {
 
+    // Prepare mock
+    @Mock
+    TqsBasicHttpClient httpClient;
+
+    // Create SUT and inject mock (IoC)
+    @InjectMocks
+    AddressResolver resolver;
 
     @Test
     void whenGoodAlboiGps_returnAddress() throws ParseException, IOException, URISyntaxException {
 
-        //todo
+        // Mock stubbing
+        when(httpClient.get("http://open.mapquestapi.com/geocoding/v1/reverse?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&location=40.640661%2C-8.656688&includeRoadMetadata=true")).thenReturn(
+                "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2021 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2021 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.640661,\"lng\":-8.656688}},\"locations\":[{\"street\":\"Cais do Alboi\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3800-246\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"displayLatLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.64052368145179,-8.656712986761146|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-824874985\",\"roadMetadata\":null}]}]}"
+        );
 
-        //test
-        // Address result = resolver.findAddressForLocation(40.640661, -8.656688);
+        // Test
+        Address result = resolver.findAddressForLocation(40.640661, -8.656688);
 
-        //return
-        // assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
+        // Return
+        assertEquals(new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null), result);
 
     }
 
     @Test
     public void whenBadCoordidates_throwBadArrayindex() throws IOException, URISyntaxException, ParseException {
 
-        //todo
     }
 }
