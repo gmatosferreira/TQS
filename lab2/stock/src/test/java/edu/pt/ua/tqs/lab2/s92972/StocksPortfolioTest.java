df0fd025b17f7d74383eb6c1eb41befac4a1f78a
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-03-27 13:20:57 +0000

    Lab2 1a. b) Mock test for getTotalValue() with Mockito

--- lab2/stock/src/test/java/edu/pt/ua/tqs/lab2/s92972/StocksPortfolioTest.java
+++ lab2/stock/src/test/java/edu/pt/ua/tqs/lab2/s92972/StocksPortfolioTest.java
@@ -1,29 +1,38 @@
 package edu.pt.ua.tqs.lab2.s92972;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class StocksPortfolioTest {
 
-    private StocksPortfolio portfolio;
+    // Prepare mock
+    @Mock
+    IStockMarket market;
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-        this.portfolio = new StocksPortfolio();
-        this.portfolio.setMarketService(new IStockMarket());
-    }
+    // Create SUT and inject mock (IoC)
+    @InjectMocks
+    StocksPortfolio portfolio;
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-        this.portfolio = null;
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void getTotalValue() {
+        // Mockito stubbing
+        when(market.getPrice("CTT")).thenReturn(3.29);
+        when(market.getPrice("Corticeira Amorim")).thenReturn(10.14);
+
         // Add stocks to portfolio
-        this.portfolio.addStock(new Stock("CTT", 20)); // 3,29
+        this.portfolio.addStock(new Stock("CTT", 20));
+
+        // Verify  getValue
         assertEquals(65.8, this.portfolio.getTotalValue(), "getTotalValue: Value does not match expected");
 
-        this.portfolio.addStock(new Stock("Corticeira Amorim", 500)); // 10,14
+        this.portfolio.addStock(new Stock("Corticeira Amorim", 50));
         assertEquals(572.8, this.portfolio.getTotalValue(), "getTotalValue: Value does not match expected");
     }
 }
