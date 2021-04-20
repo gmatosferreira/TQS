825d763182b26a2a39ee385248de9cf533a13841
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-20 12:04:50 +0100

    Lab5 1. Project set up

---
+++ lab5/cucumber/src/main/java/edu/pt/ua/tqs/lab5/s92972/App.java
@@ -0,0 +1,13 @@
+package edu.pt.ua.tqs.lab5.s92972;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
