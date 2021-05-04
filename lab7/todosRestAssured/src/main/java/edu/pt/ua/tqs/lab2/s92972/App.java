7fafce165a0819a5a30bd9e57282da5afcab3fb5
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-04 11:37:51 +0100

    Lab7 1. Maven project set up

---
+++ lab7/todosRestAssured/src/main/java/edu/pt/ua/tqs/lab2/s92972/App.java
@@ -0,0 +1,13 @@
+package edu.pt.ua.tqs.lab2.s92972;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
