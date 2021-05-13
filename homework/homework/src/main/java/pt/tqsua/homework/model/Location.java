b1102a1a1f8cc6cae9c3af1a4311a793cccce9ff
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-13 12:21:49 +0100

    small fix

--- homework/homework/src/main/java/pt/tqsua/homework/model/Location.java
+++ homework/homework/src/main/java/pt/tqsua/homework/model/Location.java
@@ -3,9 +3,6 @@ package pt.tqsua.homework.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Location {
 
