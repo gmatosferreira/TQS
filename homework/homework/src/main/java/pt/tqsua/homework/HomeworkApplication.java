f274c4af11fa4f04da4d40d3cdd7510a67b156cc
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-13 09:31:32 +0100

    Testing Jenkins pipeline integration

--- homework/homework/src/main/java/pt/tqsua/homework/HomeworkApplication.java
+++ homework/homework/src/main/java/pt/tqsua/homework/HomeworkApplication.java
@@ -19,6 +19,7 @@ public class HomeworkApplication {
 	private static final Logger log = LoggerFactory.getLogger(HomeworkApplication.class);
 
 	public static void main(String[] args) {
+		System.out.println("Testing Jenkins pipeline integration...");
 		SpringApplication.run(HomeworkApplication.class, args);
 	}
 
