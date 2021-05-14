f66e3b482eeaceb32271b853201327857219cc11
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-14 16:48:05 +0100

    ViewController associated port number to context for test variable ports

--- homework/homework/src/main/java/pt/tqsua/homework/views/ViewController.java
+++ homework/homework/src/main/java/pt/tqsua/homework/views/ViewController.java
@@ -1,6 +1,10 @@
 package pt.tqsua.homework.views;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class ViewController {
 
+    @Autowired
+    private ServletWebServerApplicationContext webServerAppCtxt;
+
     @RequestMapping("/")
-    public String homepage() {
+    public String homepage(Model model) {
+        model.addAttribute("runningPort", webServerAppCtxt.getWebServer().getPort());
         System.out.println("/");;
         return "index";
     }
